d227aed6e7eafc060d82e3968a1621838447f037
Graham Coombe <graham.coombe@enernoc.com>
2016-08-01 17:44:47 -0700

    Requests as a dependency

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+version = "0.1.0"
+
+setup(
+    name='OSMParser',
+    version=version,
+    author='Graham Coombe',
+    author_email='gscoombe@gmail.com',
+    install_requires=[
+        "requests==2.10.0"
+    ]
+)
