1e32f0ca60c5843716199b84adcc1341ac83b0d5
Graham Coombe <graham.coombe@enernoc.com>
2016-08-03 00:02:17 -0700

    Test splitting multi-node ways if groing through intersection
    If the way is multi-node but doesn't go through an intersection we don't need to split.  The first node is the head and the last is the tail

--- tests/test_osm_graph.py
+++ tests/test_osm_graph.py
@@ -25,3 +25,52 @@ class TestOSMGraph(unittest.TestCase):
         self.assertEqual(sorted(list(graph.ways.values())), sorted(expected_ways))
         self.assertEqual(sorted(list(graph.nodes.values())), sorted(expected_nodes))
 
+    def test_ways_with_multi_nodes_arent_split_if_no_interestctions(self):
+        xml_string = """<?xml version="1.0" encoding="UTF-8"?>
+            <osm version="0.6" generator="OpenStreetMap server" copyright="OpenStreetMap and contributors" attribution="http://www.openstreetmap.org/copyright" license="http://opendatacommons.org/licenses/odbl/1-0/">
+              <bounds minlat="49.2756" minlon="-123.12518" maxlat="49.277574" maxlon="-123.122176"/>
+              <node id="5902860" changeset="8212" timestamp="2011-02-03T00:45:17Z" version="1" visible="true" user="rus" uid="96" lat="49.2759" lon="-123.1242"/>
+              <node id="5899705" changeset="8212" timestamp="2011-02-03T00:45:26Z" version="1" visible="true" user="rus" uid="96" lat="49.2766" lon="-123.1231"/>
+              <node id="5902472" changeset="8212" timestamp="2011-02-03T00:46:00Z" version="1" visible="true" user="rus" uid="96" lat="49.2748" lon="-123.1258"/>
+                <way id="201058" changeset="8212" timestamp="2011-02-03T00:48:37Z" version="1" visible="true" user="rus" uid="96">
+                <nd ref="5902860"/>
+                <nd ref="5899705"/>
+                <nd ref="5902472"/>
+              </way>
+            </osm>
+            """
+
+        graph = OSMGraph(xml_string)
+        expected_ways = [OSMWay("201058-0", ["5902860", "5899705", "5902472"])]
+        expected_nodes = [OSMNode("5902860", -123.1242, 49.2759,), OSMNode("5899705", -123.1231, 49.2766), OSMNode("5902472", -123.1258, 49.2748)]
+
+        self.assertEqual(sorted(list(graph.ways.values())), sorted(expected_ways))
+        self.assertEqual(sorted(list(graph.nodes.values())), sorted(expected_nodes))
+
+    def test_split_way_if_multi_node_through_intersection(self):
+        xml_string = """<?xml version="1.0" encoding="UTF-8"?>
+            <osm version="0.6" generator="OpenStreetMap server" copyright="OpenStreetMap and contributors" attribution="http://www.openstreetmap.org/copyright" license="http://opendatacommons.org/licenses/odbl/1-0/">
+              <bounds minlat="49.2756" minlon="-123.12518" maxlat="49.277574" maxlon="-123.122176"/>
+              <node id="5902860" changeset="8212" timestamp="2011-02-03T00:45:17Z" version="1" visible="true" user="rus" uid="96" lat="49.2759" lon="-123.1242"/>
+              <node id="5899705" changeset="8212" timestamp="2011-02-03T00:45:26Z" version="1" visible="true" user="rus" uid="96" lat="49.2766" lon="-123.1231"/>
+              <node id="5902472" changeset="8212" timestamp="2011-02-03T00:46:00Z" version="1" visible="true" user="rus" uid="96" lat="49.2748" lon="-123.1258"/>
+              <node id="553185065" visible="true" version="4" changeset="22033242" timestamp="2014-04-29T22:53:06Z" uid="1355239" lat="49.2775075" lon="-123.1267388"/>
+                <way id="201058" changeset="8212" timestamp="2011-02-03T00:48:37Z" version="1" visible="true" user="rus" uid="96">
+                    <nd ref="5902860"/>
+                    <nd ref="5899705"/>
+                    <nd ref="5902472"/>
+                </way>
+                <way id="301878980" visible="true" version="2" changeset="33257926" timestamp="2015-08-11T05:08:00Z" user="fmarier" uid="24555">
+                    <nd ref="5899705"/>
+                    <nd ref="553185065"/>
+                </way>
+            </osm>
+            """
+
+        graph = OSMGraph(xml_string)
+        expected_ways = [OSMWay("201058-0", ["5902860", "5899705"]), OSMWay("201058-1", ["5899705", "5902472"]), OSMWay("301878980-0", ["5899705", "553185065"])]
+        expected_nodes = [OSMNode("5902860", -123.1242, 49.2759,), OSMNode("5899705", -123.1231, 49.2766), OSMNode("5902472", -123.1258, 49.2748), OSMNode("553185065", -123.1267388, 49.2775075)]
+
+        self.assertEqual(sorted(list(graph.ways.values())), sorted(expected_ways))
+        self.assertEqual(sorted(list(graph.nodes.values())), sorted(expected_nodes))
+
