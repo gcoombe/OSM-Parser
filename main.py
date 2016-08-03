3a6f8856752821905e9c3e671da2f01dbacb4cc4
Graham Coombe <graham.coombe@enernoc.com>
2016-08-02 22:57:21 -0700

    unintended commit in main:x

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python3
 
 import fetcher
-import graph
-
 
 def main():
-    response = fetcher.fetch_bounded_box_map(-123.125180, 49.275600, -123.122176, 49.277574)
+    response = fetcher.fetch_bounded_box_map(-123.231508, 49.257094, -123.196980, 49.25777)
     print(response)
     print(response.text)
-    graph.Os
 
 if __name__ == '__main__':
     main()
