e5ba379f778c6fd12e522b27a6a007109e88364d
Graham Coombe <gscoombe@gmail.com>
2016-10-25 15:42:18 -0700

    Fetch via overpass api

--- osmparser/fetcher.py
+++ osmparser/fetcher.py
@@ -1,5 +1,5 @@
 """
-Functions to fetch data from OSM api
+Functions to fetch data from OSM overpass_api
 
 """
 
@@ -8,15 +8,26 @@ import os
 import json
 from .graph import Graph
 from .osm_graph import OSMGraph
+import overpy
 
 from requests.auth import HTTPBasicAuth
 
-OSM_BASE_URL = 'http://api06.dev.openstreetmap.org/'
+OSM_BASE_URL = 'http://overpass_api06.dev.openstreetmap.org/'
 
+OVERPASS_QUERY_TEMPLATE = """way[highway!=service][!building]({0})->.w;
+    node({0})->.n;
+    .w>->.nw;
+    (
+      .w;
+      node.nw.n;
+    );
+    out meta;"""
+
+overpass_api = overpy.Overpass()
 
 def fetch_bounded_box_map(left, bottom, right, top):
     bounding_box = ','.join(map(str, (left, bottom, right, top)))
-    return _request("GET", OSM_BASE_URL + '/api/0.6/map', params={'bbox': bounding_box})
+    return _request("GET", OSM_BASE_URL + '/overpass_api/0.6/map', params={'bbox': bounding_box})
 
 def fetch_bounded_box_graph(left, bottom, right, top):
     fetch_reponse = fetch_bounded_box_map(left, bottom, right, top)
@@ -43,3 +54,15 @@ def _request(method, url, params=None, data=None, timeout=60):
     except requests.RequestException as e:
         print('Failure to fetch {}, {}'.format(url, e))
         raise e
+
+def _build_overpass_query(left, bottom, right, top):
+    bounding_box = ','.join(map(str, (bottom, left, top, right)))
+    return OVERPASS_QUERY_TEMPLATE.format(bounding_box)
+
+def fetch_from_overpass(left, bottom, right, top):
+    return overpass_api.query(_build_overpass_query(left, bottom, right, top))
+
+def fetch_osm_graph_from_overpass(left, bottom, right, top):
+    fetch_reponse = fetch_from_overpass(left, bottom, right, top)
+    osm_graph = OSMGraph.from_overpy_result(fetch_reponse)
+    return Graph.from_osm_graph(osm_graph)
