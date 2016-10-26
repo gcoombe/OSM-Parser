1786e6abd3f61abff263488282cd476a5586f79c
Graham Coombe <gscoombe@gmail.com>
2016-10-25 18:55:50 -0700

    Single entry point for fetching a graph
    Can specify overpass or osm to control which api is used

--- osmparser/fetcher.py
+++ osmparser/fetcher.py
@@ -25,11 +25,20 @@ OVERPASS_QUERY_TEMPLATE = """way[highway!=service][!building]({0})->.w;
 
 overpass_api = overpy.Overpass()
 
-def fetch_bounded_box_map(left, bottom, right, top):
+"""
+Fetch a graph representation of the map specified by the bounded box.
+
+provider can be "overpass" or "osm".  overpass is suggested its api is designed for querying an superior than the base osm api.
+
+"""
+def fetch_bounded_box_graph(left, bottom, right, top, provider="overpass"):
+
+
+def _fetch_bounded_box_map(left, bottom, right, top):
     bounding_box = ','.join(map(str, (left, bottom, right, top)))
     return _request("GET", OSM_BASE_URL + '/api/0.6/map', params={'bbox': bounding_box})
 
-def fetch_bounded_box_graph(left, bottom, right, top):
+def _fetch_bounded_box_graph_from_osm(left, bottom, right, top):
     fetch_reponse = fetch_bounded_box_map(left, bottom, right, top)
     osm_graph = OSMGraph.from_xml_data(fetch_reponse.text)
     return Graph.from_osm_graph(osm_graph)
@@ -59,10 +68,10 @@ def _build_overpass_query(left, bottom, right, top):
     bounding_box = ','.join(map(str, (bottom, left, top, right)))
     return OVERPASS_QUERY_TEMPLATE.format(bounding_box)
 
-def fetch_from_overpass(left, bottom, right, top):
+def _fetch_from_overpass(left, bottom, right, top):
     return overpass_api.query(_build_overpass_query(left, bottom, right, top))
 
-def fetch_osm_graph_from_overpass(left, bottom, right, top):
-    fetch_reponse = fetch_from_overpass(left, bottom, right, top)
+def _fetch_osm_graph_from_overpass(left, bottom, right, top):
+    fetch_reponse = _fetch_from_overpass(left, bottom, right, top)
     osm_graph = OSMGraph.from_overpy_result(fetch_reponse)
     return Graph.from_osm_graph(osm_graph)
