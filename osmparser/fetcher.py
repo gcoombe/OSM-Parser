805808e3228b3c266d6fae43ab6bd2da1f09dc9b
Graham Coombe <gscoombe@gmail.com>
2016-10-25 22:03:02 -0700

    Single entry point for fetcher

--- osmparser/fetcher.py
+++ osmparser/fetcher.py
@@ -14,14 +14,14 @@ from requests.auth import HTTPBasicAuth
 
 OSM_BASE_URL = 'http://api06.dev.openstreetmap.org/'
 
-OVERPASS_QUERY_TEMPLATE = """way[highway!=service][!building]({0})->.w;
+OVERPASS_QUERY_TEMPLATE = """way[highway][highway!=service][!building][!junction]({0})->.w;
     node({0})->.n;
     .w>->.nw;
     (
       .w;
       node.nw.n;
     );
-    out meta;"""
+    out;"""
 
 overpass_api = overpy.Overpass()
 
@@ -32,14 +32,13 @@ provider can be "overpass" or "osm".  overpass is suggested its api is designed 
 
 """
 def fetch_bounded_box_graph(left, bottom, right, top, provider="overpass"):
-
-
-def _fetch_bounded_box_map(left, bottom, right, top):
-    bounding_box = ','.join(map(str, (left, bottom, right, top)))
-    return _request("GET", OSM_BASE_URL + '/api/0.6/map', params={'bbox': bounding_box})
+    if provider == "osm":
+        return _fetch_bounded_box_graph_from_osm(left, bottom, right, top)
+    return _fetch_graph_from_overpass(left, bottom, right, top);
 
 def _fetch_bounded_box_graph_from_osm(left, bottom, right, top):
-    fetch_reponse = fetch_bounded_box_map(left, bottom, right, top)
+    bounding_box = ','.join(map(str, (left, bottom, right, top)))
+    fetch_reponse = _request("GET", OSM_BASE_URL + '/api/0.6/map', params={'bbox': bounding_box})
     osm_graph = OSMGraph.from_xml_data(fetch_reponse.text)
     return Graph.from_osm_graph(osm_graph)
 
@@ -64,14 +63,12 @@ def _request(method, url, params=None, data=None, timeout=60):
         print('Failure to fetch {}, {}'.format(url, e))
         raise e
 
-def _build_overpass_query(left, bottom, right, top):
-    bounding_box = ','.join(map(str, (bottom, left, top, right)))
-    return OVERPASS_QUERY_TEMPLATE.format(bounding_box)
-
-def _fetch_from_overpass(left, bottom, right, top):
-    return overpass_api.query(_build_overpass_query(left, bottom, right, top))
-
-def _fetch_osm_graph_from_overpass(left, bottom, right, top):
+def _fetch_graph_from_overpass(left, bottom, right, top):
     fetch_reponse = _fetch_from_overpass(left, bottom, right, top)
     osm_graph = OSMGraph.from_overpy_result(fetch_reponse)
     return Graph.from_osm_graph(osm_graph)
+
+def _fetch_from_overpass(left, bottom, right, top):
+    bounding_box = ','.join(map(str, (bottom, left, top, right)))
+    print(OVERPASS_QUERY_TEMPLATE.format(bounding_box))
+    return overpass_api.query(OVERPASS_QUERY_TEMPLATE.format(bounding_box))
